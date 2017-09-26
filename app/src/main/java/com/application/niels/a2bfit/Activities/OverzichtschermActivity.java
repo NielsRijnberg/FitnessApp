038769675dce7f87e42a42e7e64f2c22d40f48b7
Niels Rijnberg <360462@student.fontys.nl>
2017-09-26 13:25:47 +0200

    Navigatie gefixt, met doorverwijzingen naar nieuwe pagina's. activities toegevoegd

--- app/src/main/java/com/application/niels/a2bfit/Activities/OverzichtschermActivity.java
+++ app/src/main/java/com/application/niels/a2bfit/Activities/OverzichtschermActivity.java
@@ -1,11 +1,14 @@
 package com.application.niels.a2bfit.Activities;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.application.niels.a2bfit.R;
 
@@ -27,6 +30,69 @@ public class OverzichtschermActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                mDrawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()){
+
+
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_Producten:
+                        Toast.makeText(getApplicationContext(), "Producten", Toast.LENGTH_SHORT).show();
+                        Intent productIntent = new Intent(OverzichtschermActivity.this, ProductActivity.class);
+                        startActivity(productIntent);
+                        return true;
+                    case R.id.nav_Aankopen:
+                        Toast.makeText(getApplicationContext(), "Aankopen", Toast.LENGTH_SHORT).show();
+                        Intent aankopenIntent = new Intent(OverzichtschermActivity.this, AankopenActivity.class);
+                        startActivity(aankopenIntent);
+                        return true;
+                    case R.id.nav_Account:
+                        Toast.makeText(getApplicationContext(), "Account", Toast.LENGTH_SHORT).show();
+                        Intent accountIntent = new Intent(OverzichtschermActivity.this, AccountActivity.class);
+                        startActivity(accountIntent);
+                        return true;
+                    case R.id.nav_Instellingen:
+                        Toast.makeText(getApplicationContext(), "Instellingen", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_Oefeningen:
+                        Toast.makeText(getApplicationContext(), "Oefeningen", Toast.LENGTH_SHORT).show();
+                        Intent oefeningenIntent = new Intent(OverzichtschermActivity.this, OefeningenActivity.class);
+                        startActivity(oefeningenIntent);
+                        return true;
+                    case R.id.nav_Schemas:
+                        Toast.makeText(getApplicationContext(), "Schema's", Toast.LENGTH_SHORT).show();
+                        Intent schemasIntent = new Intent(OverzichtschermActivity.this, SchemasActivity.class);
+                        startActivity(schemasIntent);
+                        return true;
+                    case R.id.nav_Uitloggen:
+                        Toast.makeText(getApplicationContext(), "Uitloggen", Toast.LENGTH_SHORT).show();
+                        Intent uitloggenIntent = new Intent(OverzichtschermActivity.this, LoginActivity.class);
+                        startActivity(uitloggenIntent);
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Somethings Wrong", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+            }
+        });
     }
 
     @Override
@@ -35,7 +101,6 @@ public class OverzichtschermActivity extends AppCompatActivity {
         if(mDrawerToggle.onOptionsItemSelected(item)){
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/application/niels/a2bfit/Activities/ProductActivity.java
@@ -0,0 +1,15 @@
+package com.application.niels.a2bfit.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.application.niels.a2bfit.R;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
