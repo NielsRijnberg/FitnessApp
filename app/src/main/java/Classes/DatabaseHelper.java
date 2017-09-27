b485ca26575bca34c384765d6708b2264bda2035
Niels Rijnberg <360462@student.fontys.nl>
2017-09-27 12:27:05 +0200

    database klasse toegevoegd met abonnee tabel

---
+++ app/src/main/java/Classes/DatabaseHelper.java
@@ -0,0 +1,33 @@
+package Classes;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Sportschool.db";
+    public static final String TABLE_NAME = "Abonnee_Table";
+    public static final String COLUMN_ABONNEEID = "AbonneeID";
+    public static final String COLUMN_NAAM = "Naam";
+    public static final String COLUMN_WACHTOORD = "Wachtwoord";
+    public static final String COLUMN_LEEFTIJD = "Leeftijd";
+    public static final String COLUMN_MAILADRES = "Mailadres";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //van de int kosten moet nog een decimal gemaakt worden
+        db.execSQL("create table " + TABLE_NAME + " (AbonneeID INTEGER PRIMARY KEY AUTOINCREMENT, Naam TEXT, Wachtwoord TEXT, Leeftijd INTEGER, Mailadres TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/application/niels/a2bfit/Activities/ProductActivity.java
+++ app/src/main/java/com/application/niels/a2bfit/Activities/ProductActivity.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 
 import com.application.niels.a2bfit.R;
 
+import Classes.DatabaseHelper;
+
 public class ProductActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        myDb = new DatabaseHelper(this);
     }
 }
