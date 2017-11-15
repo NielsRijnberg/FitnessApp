61a9322bd49ff85c8ba9bdc5cbfd6da05da4d9e6
Niels Rijnberg <360462@student.fontys.nl>
2017-11-15 14:40:29 +0100

    datepicker gefixt, commit voor ik de database aan ga passen, aantalsets en aantal reps moeten bij een oefening op een schema komen te staan en niet bij oefening zelf

--- app/src/main/java/com/application/niels/a2bfit/Activities/StartTrainingMetSchemaActivity.java
+++ app/src/main/java/com/application/niels/a2bfit/Activities/StartTrainingMetSchemaActivity.java
@@ -1,9 +1,12 @@
 package com.application.niels.a2bfit.Activities;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,12 +15,13 @@ import com.application.niels.a2bfit.R;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class StartTrainingMetSchemaActivity extends AppCompatActivity {
 
     TextView tvSchemaType;
     EditText etDatum;
-    String dateFromDatePicker;
+    Calendar calendar;
 
 
     @Override
@@ -29,26 +33,35 @@ public class StartTrainingMetSchemaActivity extends AppCompatActivity {
         etDatum = (EditText) findViewById(R.id.etDatum);
         etDatum.setFocusable(false);
         etDatum.setClickable(true);
+        calendar = Calendar.getInstance();
 
         goToDatePicker();
-        getDateFromDatePicker();
     }
 
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener(){
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
 
     public void goToDatePicker(){
         etDatum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent datePickerIntent = new Intent(StartTrainingMetSchemaActivity.this, PickDateActivity.class);
-                startActivity(datePickerIntent);
+                new DatePickerDialog(StartTrainingMetSchemaActivity.this,R.style.DatePickerTheme, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
     }
 
-    public void getDateFromDatePicker(){
-        if (getIntent().hasExtra("datumVanOefening")){
-            dateFromDatePicker = getIntent().getExtras().getString("datumVanOefening");
-            etDatum.setText(dateFromDatePicker);
-        }
+    public void updateLabel(){
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.GERMANY);
+
+        etDatum.setText(sdf.format(calendar.getTime()));
     }
 }
