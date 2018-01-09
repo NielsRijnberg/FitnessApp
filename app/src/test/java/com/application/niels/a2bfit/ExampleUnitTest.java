6cb077929431017e353836209798b8bca3e848e1
Niels Rijnberg <360462@student.fontys.nl>
2018-01-09 21:39:00 +0100

    Commit voor de aanpassing met de unit tests en aanpassing van de activities

--- app/src/test/java/com/application/niels/a2bfit/ExampleUnitTest.java
+++ app/src/test/java/com/application/niels/a2bfit/ExampleUnitTest.java
@@ -4,14 +4,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void getOefeningen_Borstschema_ReturnsOefeningen() throws Exception {
+
         assertEquals(4, 2 + 2);
     }
 }
